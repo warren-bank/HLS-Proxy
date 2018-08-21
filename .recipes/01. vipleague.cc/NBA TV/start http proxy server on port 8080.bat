2ba8bdc65a6ace10db338fed685ed45cad550730
Warren R Bank <warren.r.bank@gmail.com>
2018-08-20 21:38:34 -0700

    recipes: recategorize, and add additional streams

--- .recipes/01. vipleague.cc/NBA TV/start http proxy server on port 8080.bat
+++ .recipes/01. vipleague.cc/NBA TV/start http proxy server on port 8080.bat
@@ -2,9 +2,9 @@
 
 start "keep alive session authorization" cmd /c "%~dp0.\auth\keep_authorization.bat"
 
-set port=8081
+set port=8080
 set req_headers="%~dp0.\req-headers.json"
 
-set hlsd_js="%~dp0..\..\hls-proxy\bin\hlsd.js"
+set hlsd_js="%~dp0..\..\..\hls-proxy\bin\hlsd.js"
 
-node %hlsd_js% -v 1 --tls --port %port% --req-headers %req_headers%
+node %hlsd_js% -v 1 --port %port% --req-headers %req_headers%
---
+++ .recipes/01. vipleague.cc/NBA TV/start https proxy server on port 8081.bat
@@ -0,0 +1,10 @@
+@echo off
+
+start "keep alive session authorization" cmd /c "%~dp0.\auth\keep_authorization.bat"
+
+set port=8081
+set req_headers="%~dp0.\req-headers.json"
+
+set hlsd_js="%~dp0..\..\..\hls-proxy\bin\hlsd.js"
+
+node %hlsd_js% -v 1 --tls --port %port% --req-headers %req_headers%
