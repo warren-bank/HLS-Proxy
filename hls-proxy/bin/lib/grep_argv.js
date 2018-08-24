35c4094798c3e455a639406dfed56d4f8618fe5d
Warren R Bank <warren.r.bank@gmail.com>
2018-08-24 03:11:20 -0700

    refactoring: trivial

--- hls-proxy/bin/lib/grep_argv.js
+++ hls-proxy/bin/lib/grep_argv.js
@@ -3,28 +3,28 @@ const fs = require('fs')
 // assumes that all arguments are passed with the convention: -key value
 // where "-key" is a flag
 
-const retrieve_flag_value = function(flags, args, flag, index) {
+const retrieve_flag_value = function(flag_opts, args, index) {
   let val
 
   if (index >= 0) {
-    if (flags[flag] && flags[flag]["bool"]) {
+    if (flag_opts && flag_opts["bool"]) {
       val = true
     }
     else if (index + 1 < args.length) {
       val = args[index + 1]
 
-      if (flags[flag] && flags[flag]["num"]) {
+      if (flag_opts && flag_opts["num"]) {
         val = Number(val)
 
         if (isNaN(val)) val = ""
       }
 
-      if (flags[flag] && flags[flag]["file"]) {
+      if (flag_opts && flag_opts["file"]) {
         try {
           val = fs.realpathSync(val, {encoding: 'utf8'})
           val = fs.readFileSync(val, {encoding: 'utf8'})
 
-          if ( (typeof flags[flag]["file"] === "string") && (flags[flag]["file"].toLowerCase() === "json") ) {
+          if ( (typeof flag_opts["file"] === "string") && (flag_opts["file"].toLowerCase() === "json") ) {
             val = JSON.parse(val)
           }
         }
@@ -46,12 +46,13 @@ const grep_argv = function(flags) {
 
   if (args.length > 0) {
     (Object.keys(flags)).forEach((flag) => {
-      let is_array = (flags[flag] && flags[flag]["many"])
-      let index = args.indexOf(flag)
+      let flag_opts = flags[flag]
+      let is_array  = flag_opts && flag_opts["many"]
+      let index     = args.indexOf(flag)
       let val
 
       if (!is_array) {
-        val = retrieve_flag_value(flags, args, flag, index)
+        val = retrieve_flag_value(flag_opts, args, index)
 
         if (val !== undefined) {
           vals[flag] = val
@@ -61,7 +62,7 @@ const grep_argv = function(flags) {
         vals[flag] = []
 
         while (index >=0) {
-          val = retrieve_flag_value(flags, args, flag, index)
+          val = retrieve_flag_value(flag_opts, args, index)
 
           if (val !== undefined) {
             vals[flag].push(val)
