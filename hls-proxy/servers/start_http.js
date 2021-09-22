25f15c4d61f6bd79e48aabf23377ad07def6569e
the-darkvoid <the-darkvoid@users.noreply.github.com>
2021-09-22 19:58:41 +0400

    Added --inline-key commandline argument

--- hls-proxy/servers/start_http.js
+++ hls-proxy/servers/start_http.js
@@ -2,13 +2,13 @@ const resolve = require('./lib/LAN_IPs').resolve
 const proxy   = require('../proxy')
 const http    = require('http')
 
-const start_server = function({host, port, req_headers, req_options, hooks, cache_segments, max_segments, cache_timeout, cache_key, verbosity, acl_whitelist}) {
+const start_server = function({host, port, req_headers, req_options, hooks, cache_segments, max_segments, cache_timeout, cache_key, inline_keys, verbosity, acl_whitelist}) {
   if (!port || isNaN(port)) port = 80
 
   resolve(host, port)
   .then((host) => {
     const server = http.createServer()
-    proxy({server, host, is_secure: false, req_headers, req_options, hooks, cache_segments, max_segments, cache_timeout, cache_key, debug_level: verbosity, acl_whitelist})
+    proxy({server, host, is_secure: false, req_headers, req_options, hooks, cache_segments, max_segments, cache_timeout, cache_key, inline_keys, debug_level: verbosity, acl_whitelist})
     server.listen(port, function () {
       console.log(`HTTP server is listening at: ${host}`)
     })
