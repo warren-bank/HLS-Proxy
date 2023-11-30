5cb68894c38b3dfc0f947b48184d925bb99cd2a9
Warren R Bank <warren.r.bank@gmail.com>
2023-11-30 03:32:30 -0800

    update recipe for hooks: URL encryption w/ "redirect" and "rewrite"

--- .recipes/00. generic use cases/configure hooks/url_obfuscation/.test/test.sh
+++ .recipes/00. generic use cases/configure hooks/url_obfuscation/.test/test.sh
@@ -7,26 +7,20 @@ referer_url='https://www.cbsnews.com/live/'
 
 # --------------------------------------------------------------------
 
-if [ -z "$referer_url" ];then
-  original_url="$video_url"
-else
-  original_url="${video_url}|${referer_url}"
-fi
-
 hooks_js=$(realpath '../data/hooks')
 hooks_js="${hooks_js//\\/\/}"
 
-encrypted_url=$(node -e "const hooks = require('${hooks_js}'); process.stdout.write(hooks.redirect('${original_url}'));")
+encrypted_url=$(node -e "const hooks = require('${hooks_js}'); process.stdout.write(hooks.redirect('${video_url}'));")
 cleartext_url=$(node -e "const hooks = require('${hooks_js}'); process.stdout.write(hooks.rewrite('${encrypted_url}'));")
 
-if [ ! "$original_url" == "$encrypted_url" ];then
+if [ ! "$video_url" == "$encrypted_url" ];then
   echo 'encryption: OK'
 else
   echo 'encryption: FAIL'
 fi
 
 
-if [ "$original_url" == "$cleartext_url" ];then
+if [ "$video_url" == "$cleartext_url" ];then
   echo 'roundtrip: OK'
 else
   echo 'roundtrip: FAIL'
@@ -37,10 +31,19 @@ fi
 proxy_url='http://127.0.0.1:8080'
 file_extension='.m3u8'
 
-hls_proxy_url="${proxy_url}/"$(echo -n "$encrypted_url" | base64 --wrap=0)"$file_extension"
+if [ -z "$referer_url" ];then
+  unencoded_url="$encrypted_url"
+else
+  unencoded_url="${encrypted_url}|${referer_url}"
+fi
+
+base64_encoded_url=$(echo -n "$unencoded_url" | base64 --wrap=0)
+
+hls_proxy_url="${proxy_url}/${base64_encoded_url}${file_extension}"
 
 # --------------------------------------------------------------------
 
 echo -e "cleartext_url:\n  ${cleartext_url}"
 echo -e "encrypted_url:\n  ${encrypted_url}"
+echo -e "unencoded_url:\n  ${unencoded_url}"
 echo -e "hls_proxy_url:\n  ${hls_proxy_url}"
